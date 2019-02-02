339fae6babcd57947f0fb1ea2fe08a2419abc612
RodBromberg <rod.bromberg@yahoo.com>
2019-02-02 12:09:14 -0500

    3 event  listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+// Your code goes here
+const destination_btn = document.querySelectorAll('.btn')
+const img_all = document.querySelectorAll('.img')
+
+
+destination_btn[0].addEventListener('click', (event) => {
+    event.stopPropagation();
+    alert('this button was clicked');
+})
+
+destination_btn[1].addEventListener('mouseenter', (event) => {
+    event.stopPropagation();
+    event.target.style.color = 'red'
+})
+
+destination_btn[1].addEventListener('mouseleave', (event) => {
+    event.stopPropagation()
+    event.target.style.color = 'white'
+})
+
+img_all[0].addEventListener('click', (event) => {
+    event.target.style.display = 'none'
+})
